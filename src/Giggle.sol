4cc2040220f9e8b64322f1b39efa4cefd7ffdb61
yebology <139052533+yebology@users.noreply.github.com>
2025-01-26 18:22:30 +0700

    restructure logic and move to GiggleService.sol

--- src/Giggle.sol
+++ src/Giggle.sol
@@ -1,104 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
+import {GiggleService} from "./GiggleService.sol";
+
 contract Giggle {
     //
-    enum ActionCategory {
-        DEPOSIT,
-        SETTLEMENT
+    GiggleService private i_giggleService;
+
+    constructor() {
+        i_giggleService = new GiggleService();
     }
 
-    mapping(string postId => uint256 fee) private s_serviceFee;
-    mapping(string postId => address wallet) private s_postCreatorWallet;
-    mapping(address buyer => mapping(string postId => uint256 depositAmount))
-        private s_buyerDepositAmount;
-    mapping(ActionCategory actionCategory => uint8 numerator) private s_actionCategoryNumerator;
-
-    error InvalidPostInput();
-    error ExistenceDepositDetected();
-    error NonExistenceDepositDetected();
-    error InvalidPaymentAmount();
-
-    constructor () {
-        s_actionCategoryNumerator[ActionCategory.DEPOSIT] = 3;
-        s_actionCategoryNumerator[ActionCategory.SETTLEMENT] = 7;
+    function registerWallet(string memory _postId) external {
+        i_giggleService.registerWallet(_postId, msg.sender);
     }
 
-    modifier checkPostInput(string memory _postId, uint256 _fee) {
-        if (bytes(_postId).length == 0 || _fee <= 0) {
-            revert InvalidPostInput();
-        }
-        _;
-    }
-
-    modifier checkDepositExistence(
+    function createProposalRequest(
         string memory _postId,
+        uint256 _daysEstimationForCompletion,
         address _buyer,
-        ActionCategory _actionCategory
-    ) {
-        uint256 depositAmount = s_buyerDepositAmount[_buyer][_postId];
-        if (_actionCategory == ActionCategory.DEPOSIT && depositAmount != 0) {
-            revert ExistenceDepositDetected();
-        }
-        else if (
-            _actionCategory == ActionCategory.SETTLEMENT && depositAmount == 0
-        ) {
-            revert NonExistenceDepositDetected();
-        }
-        _;
+        uint256 _finalFee
+    ) external {
+        i_giggleService.createProposalRequest(
+            _postId,
+            _daysEstimationForCompletion,
+            _buyer,
+            _finalFee
+        );
     }
 
-    modifier checkPaymentAmount(
-        string memory _postId,
-        uint256 _amount,
-        ActionCategory _actionCategory
-    ) {
-        uint256 serviceFee = s_serviceFee[_postId];
-        uint8 numerator = s_actionCategoryNumerator[_actionCategory];
-        uint256 expectedDepositAmount = (serviceFee * numerator) / 10;
-        if (_amount != expectedDepositAmount) {
-            revert InvalidPaymentAmount();
-        }
-        _;
+    function acceptProposalRequest(uint256 _proposalId) external payable {
+        i_giggleService.acceptProposalRequest(_proposalId, msg.sender);
     }
 
-    function registerPost(
-        string memory _postId,
-        uint256 _fee
-    ) external checkPostInput(_postId, _fee) {
-        s_serviceFee[_postId] = _fee;
-        s_postCreatorWallet[_postId] = msg.sender;
+    function finishOrder(uint256 _orderId) external {
+        i_giggleService.finishOrder(_orderId, msg.sender);
     }
 
-    function updateServiceFee(
-        string memory _postId,
-        uint256 _newFee
-    ) external checkPostInput(_postId, _newFee) {
-        s_serviceFee[_postId] = _newFee;
+    function approveFinishedOrder(uint256 _orderId) external {
+        i_giggleService.approveFinishedOrder(_orderId, msg.sender);
     }
 
-    function addDeposit(
-        string memory _postId
-    )
-        external
-        payable
-        checkDepositExistence(_postId, msg.sender, ActionCategory.DEPOSIT)
-        checkPaymentAmount(_postId, msg.value, ActionCategory.DEPOSIT)
-    {
-        s_buyerDepositAmount[msg.sender][_postId] = msg.value;
+    function withdrawFunds(uint256 _orderId) external {
+        i_giggleService.withdrawFunds(_orderId, msg.sender);
     }
-
-    function settlePayment(
-        string memory _postId
-    )
-        external
-        payable
-        checkDepositExistence(_postId, msg.sender, ActionCategory.SETTLEMENT)
-        checkPaymentAmount(_postId, msg.value, ActionCategory.SETTLEMENT)
-    {}
-
-    function withdrawFunds() external payable {}
-
-    function returnFunds() external {}
     //
 }
---
+++ src/GiggleService.sol
@@ -0,0 +1,243 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract GiggleService {
+    //
+    enum OrderStatus {
+        PAID,
+        FINISHED,
+        APPROVED,
+        FEE_WITHDRAWED,
+        CANCELED
+    }
+
+    struct Proposal {
+        uint256 proposalId;
+        string postId;
+        address buyer;
+        uint256 finalFee;
+        uint256 daysEstimationForCompletion;
+    }
+    struct Order {
+        uint256 orderId;
+        uint256 proposalId;
+        uint256 orderAcceptedAt;
+        OrderStatus status;
+    }
+
+    mapping(string postId => address owner) private s_postOwnerWallet;
+    mapping(address buyer => mapping(uint256 orderId => uint256 fund))
+        private s_fundsFromBuyer;
+
+    Proposal[] private s_proposals;
+    Order[] private s_orders;
+
+    error InvalidPostIdOrWalletInput();
+    error InvalidProposalInput();
+    error InvalidPaymentAmount();
+    error InvalidAuthorization();
+    error ExistenceFundsDetected();
+    error InvalidOrderStatus();
+    error InvalidWithdrawAction();
+
+    modifier checkPostIdAndWalletInput(string memory _postId, address _wallet) {
+        if (bytes(_postId).length == 0 || _wallet == address(0)) {
+            revert InvalidPostIdOrWalletInput();
+        }
+        _;
+    }
+
+    modifier checkPaymentAmount(uint256 _proposalId, uint256 _amount) {
+        uint256 serviceFee = s_proposals[_proposalId].finalFee;
+        if (_amount != serviceFee) {
+            revert InvalidPaymentAmount();
+        }
+        _;
+    }
+
+    modifier checkFundExistence(address _buyer, uint256 _orderId) {
+        uint256 funds = s_fundsFromBuyer[_buyer][_orderId];
+        if (funds != 0) {
+            revert ExistenceFundsDetected();
+        }
+        _;
+    }
+
+    modifier checkPostOwnerAuthorization(uint256 _orderId, address _caller) {
+        uint256 proposalId = s_orders[_orderId].proposalId;
+        string memory postId = s_proposals[proposalId].postId;
+        address expectedOwner = s_postOwnerWallet[postId];
+        if (expectedOwner != _caller) {
+            revert InvalidAuthorization();
+        }
+        _;
+    }
+
+    modifier checkUserAuthorization(uint256 _proposalId, address _caller) {
+        address expectedBuyer = s_proposals[_proposalId].buyer;
+        if (expectedBuyer != _caller) {
+            revert InvalidAuthorization();
+        }
+        _;
+    }
+
+    modifier checkApproverAuthorization(uint256 _orderId, address _caller) {
+        uint256 proposalId = s_orders[_orderId].proposalId;
+        address expectedApprover = s_proposals[proposalId].buyer;
+        if (expectedApprover != _caller) {
+            revert InvalidAuthorization();
+        }
+        _;
+    }
+
+    modifier checkOrderStatus(uint256 _orderId, OrderStatus expectedStatus) {
+        OrderStatus status = s_orders[_orderId].status;
+        if (status != expectedStatus) {
+            revert InvalidOrderStatus();
+        }
+        _;
+    }
+
+    modifier checkWithdrawStatus(uint256 _orderId, OrderStatus expectedStatus) {
+        OrderStatus status = s_orders[_orderId].status;
+        uint256 orderTimestamp = s_orders[_orderId].orderAcceptedAt;
+        uint256 proposalId = s_orders[_orderId].proposalId;
+        uint256 daysEstimation = s_proposals[proposalId]
+            .daysEstimationForCompletion * 1 days;
+        if (
+            status != expectedStatus &&
+            block.timestamp < orderTimestamp + daysEstimation
+        ) {
+            revert InvalidWithdrawAction();
+        }
+        _;
+    }
+
+    function registerWallet(
+        string memory _postId,
+        address _wallet
+    ) external checkPostIdAndWalletInput(_postId, _wallet) {
+        s_postOwnerWallet[_postId] = _wallet;
+    }
+
+    function createProposalRequest(
+        string memory _postId,
+        uint256 _daysEstimationForCompletion,
+        address _buyer,
+        uint256 _finalFee
+    ) external checkPostIdAndWalletInput(_postId, _buyer) {
+        _addNewProposal(
+            _postId,
+            _daysEstimationForCompletion,
+            _buyer,
+            _finalFee
+        );
+    }
+
+    function acceptProposalRequest(
+        uint256 _proposalId,
+        address _user
+    )
+        external
+        payable
+        checkUserAuthorization(_proposalId, _user)
+        checkPaymentAmount(_proposalId, msg.value)
+    {
+        _placeFunds(_proposalId, _user, msg.value);
+        _addNewOrder(_proposalId);
+    }
+
+    function finishOrder(
+        uint256 _orderId,
+        address _owner
+    )
+        external
+        checkPostOwnerAuthorization(_orderId, _owner)
+        checkOrderStatus(_orderId, OrderStatus.PAID)
+    {
+        s_orders[_orderId].status = OrderStatus.FINISHED;
+    }
+
+    function approveFinishedOrder(
+        uint256 _orderId,
+        address _approver
+    )
+        external
+        checkApproverAuthorization(_orderId, _approver)
+        checkOrderStatus(_orderId, OrderStatus.FINISHED)
+    {
+        s_orders[_orderId].status = OrderStatus.APPROVED;
+    }
+
+    function withdrawFunds(
+        uint256 _orderId,
+        address _owner
+    )
+        external
+        checkPostOwnerAuthorization(_orderId, _owner)
+        checkWithdrawStatus(_orderId, OrderStatus.APPROVED)
+    {
+        _withdrawFunds(_orderId, _owner);
+    }
+
+    function returnFunds() external {}
+
+    function _addNewProposal(
+        string memory _postId,
+        uint256 _daysEstimationForCompletion,
+        address _buyer,
+        uint256 _finalFee
+    ) private {
+        s_proposals.push(
+            Proposal({
+                proposalId: s_proposals.length,
+                postId: _postId,
+                buyer: _buyer,
+                daysEstimationForCompletion: _daysEstimationForCompletion,
+                finalFee: _finalFee
+            })
+        );
+    }
+
+    function _addNewOrder(uint256 _proposalId) private {
+        s_orders.push(
+            Order({
+                orderId: s_orders.length,
+                proposalId: _proposalId,
+                orderAcceptedAt: block.timestamp,
+                status: OrderStatus.PAID
+            })
+        );
+    }
+
+    function _withdrawFunds(uint256 _orderId, address _owner) private {
+        uint256 proposalId = s_orders[_orderId].proposalId;
+        uint256 withdrawAmount = s_proposals[proposalId].finalFee;
+        bool success = _transferFunds(_owner, withdrawAmount);
+        if (success) {
+            s_orders[_orderId].status = OrderStatus.FEE_WITHDRAWED;
+        }
+    }
+
+    function _placeFunds(
+        uint256 _orderId,
+        address _buyer,
+        uint256 _amount
+    ) private {
+        bool success = _transferFunds(address(this), _amount);
+        if (success) {
+            s_fundsFromBuyer[_buyer][_orderId] = msg.value;
+        }
+    }
+
+    function _transferFunds(
+        address _recipient,
+        uint256 _amount
+    ) private returns (bool) {
+        address payable recipient = payable(address(_recipient));
+        (bool success, ) = recipient.call{value: _amount}("");
+        return success;
+    }
+    //
+}
